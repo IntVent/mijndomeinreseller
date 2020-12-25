c235ef868b7b3de79f73312b616cb0d4aa4e6a7c
Peter Steenbergen <peter@3ws.nl>
2020-12-25 16:59:24 +0100

    ConnctionTest

--- tests/ConnectionTest.php
+++ tests/ConnectionTest.php
@@ -3,14 +3,15 @@
 namespace IntVent\MijnDomeinReseller\Tests;
 
 use IntVent\MijnDomeinReseller\Client;
+use IntVent\MijnDomeinReseller\Exceptions\ApiException;
 use PHPUnit\Framework\TestCase;
 
 class ConnectionTest extends TestCase
 {
     /** @test */
-    public function connection_failes_with_false_credentials()
+    public function connection_fails_with_false_credentials()
     {
-        $this->expectException(\IntVent\MijnDomeinReseller\Exceptions\ApiException::class);
+        $this->expectException(ApiException::class);
 
         $client = new Client('test', 'test');
         $client->get('domain_list');
